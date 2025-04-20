b96c706595b22abcb2ca08ed1137b3ff5894495a
tecnik <44696073+TecnikOfficial@users.noreply.github.com>
2025-04-20 15:57:51 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   // Adjust this to your repository name
-  base: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vue/" : "/",
   build: {
     // Optimize build
     minify: "terser",
@@ -24,4 +24,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
